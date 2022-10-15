ec7dbb562a53fcb09ce1dba2808905eb0055dab2
grishanova-oa <grishanova.oa@gmail.com>
2022-10-15 13:41:57 +0200

    feat: done task

--- src/stack.js
+++ src/stack.js
@@ -12,21 +12,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ class Stack {
+  elements = [];
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   push(element) {
+      this.elements.push(element);
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   };
+ 
+   pop() {
+      return this.elements.pop();
+
+   };
+ 
+   peek() {
+      return this.elements[this.elements.length - 1];
+
+   };
 }
 
 module.exports = {
